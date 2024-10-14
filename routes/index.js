e5f489696e47a8251db62770f43ecbb59883971b
justjum <jumasaurusrex@gmail.com>
2024-10-15 09:36:00 +1030

    Add Prisma lookup route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const PrismaClient = require("@prisma/client").PrismaClient;
+
+const prisma = new PrismaClient();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/books', async function(req, res, next) {
+  let books = await prisma.book.findMany({
+    include: {author: true}
+  });
+  console.log(books);
+  res.render('books', {
+    title: 'Books',
+    books: books
+  })
+})
 
 module.exports = router;
